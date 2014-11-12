baef724bd87548b67f68025842e940ad13297f95
Martin Czygan <martin.czygan@gmail.com>
2014-11-12 22:25:01 +0100

    moved SameAsHandler into handler.go

--- handlers.go
+++ handlers.go
@@ -1,10 +1,13 @@
 package proxima
 
 import (
+	"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 	"time"
 )
 
@@ -69,4 +72,56 @@ func URLHandler(options Options) http.Handler {
 		}
 	}
 	return http.HandlerFunc(fn)
+}
+
+// SameAsHandler returns the reference to VIAF and DBP for a given GND
+func SameAsHandler(options Options) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		gnd := r.URL.Path[len("/gnd/sa/"):]
+		url := fmt.Sprintf("http://%s/u?url=http://d-nb.info/gnd/%s/about/rdf", r.Host, gnd)
+		body, resp, err := Fetch(url)
+		if err != nil || resp.StatusCode >= 400 {
+			w.WriteHeader(resp.StatusCode)
+			w.Write([]byte(resp.Status))
+			return
+		}
+
+		set := make(map[string]struct{})
+		decoder := xml.NewDecoder(strings.NewReader(string(body)))
+
+		for {
+			// Read tokens from the XML document in a stream.
+			t, _ := decoder.Token()
+			if t == nil {
+				break
+			}
+			switch se := t.(type) {
+			case xml.StartElement:
+				if se.Name.Space == "http://www.w3.org/2002/07/owl#" && se.Name.Local == "sameAs" {
+					for _, attr := range se.Attr {
+						if attr.Name.Space == "http://www.w3.org/1999/02/22-rdf-syntax-ns#" && attr.Name.Local == "resource" {
+							set[attr.Value] = struct{}{}
+						}
+					}
+				}
+			default:
+			}
+		}
+		links := make([]string, 0)
+		for key := range set {
+			links = append(links, key)
+		}
+		result := make(map[string]interface{})
+		result["uri"] = fmt.Sprintf("http://d-nb.info/gnd/%s", gnd)
+		result["sa"] = links
+		b, err := json.Marshal(result)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(http.StatusText(http.StatusInternalServerError)))
+			return
+		}
+		w.Write(b)
+	}
+	return http.HandlerFunc(fn)
+
 }
