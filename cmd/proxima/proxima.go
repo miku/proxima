ae6a23c758ea3e7d7e2e48fff58fd2829ac70f7a
Martin Czygan <martin.czygan@gmail.com>
2014-11-28 12:48:01 +0100

    add support for base url path

--- cmd/proxima/proxima.go
+++ cmd/proxima/proxima.go
@@ -12,10 +12,20 @@ import (
 	"github.com/miku/proxima"
 )
 
+// prepend helps to run this service under a directory by appending a base
+// TODO: there must be support for this in mux.Handle, or not?
+func prepend(s, base string) string {
+	if base == "/" {
+		return s
+	}
+	return fmt.Sprintf("%s%s", base, s)
+}
+
 func main() {
 	addr := flag.String("addr", ":3000", "host:port to listen on")
 	cacheDir := flag.String("dir", filepath.Join(os.TempDir(), "proxima.v1"), "cache directory")
 	version := flag.Bool("v", false, "prints current program version")
+	base := flag.String("base", "/", "URL base")
 
 	flag.Parse()
 
@@ -27,9 +37,9 @@ func main() {
 	options := proxima.Options{Cache: diskcache.New(*cacheDir)}
 	mux := http.NewServeMux()
 
-	mux.Handle("/u", proxima.URLHandler(options))
-	mux.Handle("/g/s/", proxima.SameAsHandler(options))
-	mux.Handle("/g/i/", proxima.GndImageHandler(options))
+	mux.Handle(prependPrefix("/u", *base), proxima.URLHandler(options))
+	mux.Handle(prependPrefix("/g/s/", *base), proxima.SameAsHandler(options))
+	mux.Handle(prependPrefix("/g/i/", *base), proxima.GndImageHandler(options))
 
 	mux.HandleFunc("/g/r/", func(w http.ResponseWriter, r *http.Request) {
 		gnd := r.URL.Path[len("/g/r/"):]
